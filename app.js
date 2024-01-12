2cc7a3f907a96e9ae345f1c5b45a22629af6df60
rishisingh34 <singh34rishi@gmail.com>
2024-01-12 15:42:33 +0530

    Admin Model and Authentication completed

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const { PORT } = require('./config/env.config') ;
 const connectDB = require('./config/db.config');
 const trendsRoutes = require('./routes/cyber_secure.route') ;
 const authRoutes = require('./routes/auth.route') ;
+const adminRoutes = require('./routes/admin.route') ;
 const handleCors = require('./config/cors.config') ;
 const cookieParser = require('cookie-parser') ;
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
@@ -15,6 +17,7 @@ app.use(express.json()) ;
 app.use(express.urlencoded({ extended: true })) ;
 app.use('/v1', trendsRoutes) ;
 app.use('/v1/auth', authRoutes) ;
+app.use('/v1/admin', adminRoutes) ;
 app.use(handleCors) ;
 app.use('/gemini', async (req, res) => {
   try {
--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,3 +1,7 @@
 const router = require('express').Router();
+const admin = require('../controllers/admin.controller');
 
-// router.get('cases' , "")
+router.post('/login', admin.login); 
+router.post('/verifyOtp', admin.verifyOtp);
+
+module.exports = router;
