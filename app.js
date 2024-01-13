3eb68884b011b56c04c5e8ba25a87936e4aca756
rishisingh34 <singh34rishi@gmail.com>
2024-01-14 02:58:59 +0530

    minor change in cors config

--- app.js
+++ app.js
@@ -4,14 +4,11 @@ const { PORT } = require('./config/env.config') ;
 const connectDB = require('./config/db.config');
 const trendsRoutes = require('./routes/cyber_secure.route') ;
 const authRoutes = require('./routes/auth.route') ;
-const adminRoutes = require('./routes/admin.route') ;
-// const handleCors = require('./config/cors.config') ;
-const cors = require('cors');
+const handleCors = require("./config/cors.config");
 const cookieParser = require('cookie-parser') ;
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-
-
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
 
 app.use(cookieParser()) ;
 app.use(express.json()) ;
@@ -19,7 +16,8 @@ app.use(express.urlencoded({ extended: true })) ;
 app.use('/v1', trendsRoutes) ;
 app.use('/v1/auth', authRoutes) ;
 app.use('/v1/admin', adminRoutes) ;
-app.use(cors()) ;
+app.use(handleCors) ;
+
 app.use('/gemini', async (req, res) => {
   try {
     
--- config/cors.config.js
+++ config/cors.config.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 
 const corsOptions = {
-  origin: "*",
+  origin: "http://localhost:5173",
   credentials: true,
   optionsSuccessStatus: 200,
 };
