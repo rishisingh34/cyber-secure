60813bd4c78f29d6808cb8c74e4574d140b9aad2
rishisingh34 <singh34rishi@gmail.com>
2024-01-13 21:34:45 +0530

    cors error resolved

--- config/cors.config.js
+++ config/cors.config.js
@@ -1,10 +1,9 @@
 const cors = require("cors");
 
 const corsOptions = {
-  origin: "[http://localhost:5173]", 
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
+  origin: "http://localhost:5173",
+  credentials: true,
+  optionsSuccessStatus: 200,
 };
 
 const handleCors = cors(corsOptions);
