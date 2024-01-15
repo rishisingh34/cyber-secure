a6931e2ac2029d9b821f3b94d29f25a88fcacf70
rishisingh34 <singh34rishi@gmail.com>
2024-01-15 11:28:36 +0530

    minor change

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -54,7 +54,6 @@ const admin = {
       await OTP.deleteOne({ email });
       res.cookie("accessToken", accessToken, {
         httpOnly: true,
-        secure: true,
         sameSite: "none",
         domain: "cyber-secure.onrender.com",
       });
