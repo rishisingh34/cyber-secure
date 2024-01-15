b7a8f8ef95287d4e03a872dbbe2378f79b95b45c
rishisingh34 <singh34rishi@gmail.com>
2024-01-15 13:02:11 +0530

    bug fix

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -59,6 +59,8 @@ const admin = {
       await OTP.deleteOne({ email });
       res.cookie("accessToken", accessToken, {
         httpOnly: true,
+        sameSite : "none",
+        secure : true 
       });
 
 
