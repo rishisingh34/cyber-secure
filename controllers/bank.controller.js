c4b1228a2aa048299e7db1f072edaed7bf688a12
rishisingh34 <singh34rishi@gmail.com>
2024-01-18 07:50:45 +0530

    minor change in bankController

--- controllers/bank.controller.js
+++ controllers/bank.controller.js
@@ -1,34 +1,32 @@
-const Complaint = require("../models/complaint.model");
+const Complaint = require('../models/complaint.model');
 
 const bankController = {
-  freeze : async (req, res) => {
+  freeze: async (req, res) => {
     try {
-      const { acknowledgementNumber } = req.query ;
-      const complaint = await Complaint.findOneAndUpdate({ acknowledgementNumber }, { verificationStatus : true }) ;  
-      await complaint.save() ;
-      return res.render('<center><h1>Account Freezed</h1></center>') ;
+      const { acknowledgementNumber } = req.query;
+      await Complaint.findOneAndUpdate(
+        { acknowledgementNumber },
+        { verificationStatus: true }
+      );
+      return res.render("<center><h1>Account Freezed</h1></center>");
     } catch (err) {
       console.error(err);
       res.status(500).json({ message: "Internal Server Error" });
     }
   },
-  denyComplaint  : async (req, res) => {
-    try {       
-      const { acknowledgementNumber } = req.query ;       
-      const complaint = await Complaint.findOneAndUpdate({ acknowledgementNumber }, { dismissalReason : dissmissalReason , dismissalStatus : true }) ;
+  denyComplaint: async (req, res) => {
+    try {
+      const { acknowledgementNumber } = req.query;
+      await Complaint.findOneAndUpdate(
+        { acknowledgementNumber },
+        {  dismissalStatus: true }
+      );
 
-      await complaint.save() ;
-
-      return res.render('<center><h1>Complaint Denied</h1></center>') ;
-
-
-
-
+      return res.render("<center><h1>Complaint Denied</h1></center>");
     } catch (err) {
       console.error(err);
       res.status(500).json({ message: "Internal Server Error" });
     }
-  }
+  },
 };
-
-module.exports = bankController; 
+module.exports = bankController;
